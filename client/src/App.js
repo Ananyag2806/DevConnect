234d2c9ef04af0751c24e1c6d898a36b832c4ad1
Ananya Gupta <ananyag2806@gmail.com>
2022-01-16 21:30:07 +0530

    Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-// import { Landing } from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => {
     return (
-        <Fragment>
-            <Navbar />
-            <Landing />
-        </Fragment>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Landing />} />
+                </Routes>
+                <section className="container">
+                    <Routes>
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/login' element={<Login />} />
+                    </Routes>
+                </section>
+            </Fragment>
+        </Router>
     );
 }
 
